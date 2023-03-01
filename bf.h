1e9587a998f9c27c9e0c8669b0dc91a9839fabe1
anatolymedvedev <66411230+anatolymedvedev@users.noreply.github.com>
2023-03-02 02:01:08 +0700

    Create bf.h

---
+++ bf.h
@@ -0,0 +1,36 @@
+#include <string>
+#include<cstring>
+#include <algorithm> 
+#include <random>
+#include <iostream>
+#include <cmath>
+#include <bitset>
+#include <chrono>
+
+using namespace std;
+
+static std::mt19937& mt() 
+{
+    // initialize once per thread
+    thread_local static std::mt19937 mt(static_cast<uint32_t>(time(nullptr)));
+    return mt;
+}
+
+class BF
+{
+	uint32_t  n = 0;          //используемая память
+	uint32_t  len = 0;      //выделеная память
+	uint32_t* f = nullptr;	//массив коэфициентов
+public:
+    BF();
+    BF(std::string s);
+    BF(uint8_t type, uint32_t _len);
+    ~BF();
+    BF(const BF& other);
+    BF& operator =(const BF& other);
+    friend std::ostream& operator <<(std::ostream& out, BF& other);
+    uint32_t weight();
+    bool operator ==(const BF &other);
+    bool operator !=(const BF &other);
+
+};
