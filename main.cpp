272d8d1657a9587b9d425dc5f813a8ab4706a79f
Anatoly <66411230+anatolymedvedev@users.noreply.github.com>
2023-03-15 23:26:15 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@ using namespace std;
 int main()
 {
     srand((unsigned int)(time(NULL)));
+    //test_generation_balance
     // for (size_t i = 2; i < 32; i++)
     // {
     //    auto begin = std::chrono::high_resolution_clock::now();
@@ -18,6 +19,7 @@ int main()
     //    std::cout << "size_vec =" << i << "   weight/size_vec =" << (long double)(w) / (uint64_t(1) << i) << "\tTime : " << ms.count() << std::endl;
     // }
 
+    //test_Mebius
     // for (size_t i = 2; i < 32; i++)
     // {
     //    auto begin = std::chrono::steady_clock::now();
@@ -31,24 +33,22 @@ int main()
     //    end = std::chrono::steady_clock::now();
     //    auto t_mebius = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
     //    z = y.Mebius();
-       
     //    std::cout << "Arg =" << i << "\tTime_generation =" << t_gen.count() << "\tTime_mebius =" << t_mebius.count() << "\tEqual after 2 mebius? =" << ((x == z) ? "true" : "No");
     //    std::cout << std::endl;
     // }
 
     // // string str;
     // // cin >> str;
-    // BF x("0001000100011110000100010001111000010001000111101110111011100001"); //0001000100011110000100010001111000010001000111101110111011100001
-    // std::cout << x << std::endl;
+    BF x("0001000100011110000100010001111000010001000111101110111011100001"); //0000000100000001000000010000000100000001000000010000000111111110
+    std::cout << x << std::endl;
     // uint64_t w = x.weight();
     // cout << w << " - w" << endl;
-    // BF g = x.Mebius();
-    // cout << g << endl;
-    // string str = g.ANF();
-    // cout << str << endl;
-
-    int kek = log2(64); 
-    cout << kek << endl;
+    BF g = x.Mebius();
+    cout << g << endl;
+    g.ANF();
+    cout << endl;
+    uint32_t degree = g.degree_func();
+    cout << degree << endl;
 
     return 0;
 }
