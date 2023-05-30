2d651616708d2855eae317f4b2dc0a3a683ec4b8
Anatoly <66411230+anatolymedvedev@users.noreply.github.com>
2023-05-30 14:04:36 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -69,31 +69,62 @@ int main()
 	// 	std::cout << std::endl;
 	// }
 
-    // // string str;
-    // // cin >> str;
-    BF x("11010001"); //10010110
-    std::cout << x << std::endl;
+    // test_auto_cor
+    // for (size_t i = 2; i < 32; i++)
+    // {
+    //    auto begin = std::chrono::steady_clock::now();
+    //    BF x(1, (1<<i));
+    //    auto end = std::chrono::steady_clock::now();
+    //    auto t_gen = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    //    vector<int32_t> f;
+    //    begin = std::chrono::steady_clock::now();
+    //    f = x.auto_cor();
+    //    end = std::chrono::steady_clock::now();
+    //    auto t_auto_cor = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin);
+    //    std::cout << "Arg =" << i << "\tTime_generation =" << t_gen.count() << "\tauto_cor =" << t_auto_cor.count();
+    //    std::cout << std::endl;
+    // }
+
+    BF x("01101001"); //10010110  11010001 00011010
+    cout << x << endl;
     // uint64_t w = x.weight();
     // cout << w << " - w" << endl;
-    // BF g = x.Mebius();
-    // cout << g << endl;
-    // g.ANF();
-    // cout << endl;
-    // uint32_t degree = g.degree_func();
-    // cout << degree << endl;
+    BF g = x.Mebius();
+    cout << "Mebius: " << g << endl;
+    g.ANF();
+    cout << endl;
+    uint32_t degree = g.degree_func();
+    cout << "degree = " << degree << endl;
 
-    // vector<int32_t> f;
-    // f = x.Walsh_Hadamard();
-    // for (auto s : f)
-    // {
-    //     cout << s << " ";
-    // }
-    // cout << endl;
+    vector<int32_t> f;
+    f = x.Walsh_Hadamard();
+    cout << "Walsh_Hadamard: " << endl; 
+    for (auto s : f)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
 
-    // uint32_t cor = x.cor();
-    // cout << "cor = " << cor << endl;
+    uint32_t cor = x.cor();
+    cout << "cor = " << cor << endl;
 
     cout << x.nf_nap() << endl;
+    cout << endl;
+
+    vector<int32_t> AC;
+    AC = x.auto_cor();
+    cout << "Auto_cor : " << endl; 
+    for (auto s : AC)
+    {
+        cout << s << " ";
+    }
+    cout << endl;
+
+    uint32_t PC = x.PC();
+    cout << "PC = " << PC << endl;
+
+    cout << "CN_f = " << x.Cn_f() << endl;
+    cout << endl;
 
     return 0;
 }
